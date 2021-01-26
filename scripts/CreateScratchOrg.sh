4379e9b55e5603578aed62afa20bcf2facd652cb
Dave Larrimore <dlarrimore@salesforce.com>
2021-01-26 15:48:07 -0500

    Initial Commit

---
+++ scripts/CreateScratchOrg.sh
@@ -0,0 +1,29 @@
+echo "*** Creating scratch org ..."
+sfdx force:org:create -f config/project-scratch-def.json --setdefaultusername --setalias eWRTSScratch -d 30
+
+echo "*** Pushing metadata to scratch org ..."
+sfdx force:source:push
+
+echo "*** Assigning permission set to your user ..."
+#sfdx force:user:permset:assign --permsetname eWRTS
+
+echo "*** Generating password for your user ..."
+#sfdx force:user:password:generate --targetusername eWRTSScratch#
+
+echo "*** Creating data"
+#sfdx shane:data:tree:import -p data/export-demo-service_item__c-plan.json -d data/
+#sfdx shane:data:tree:import -p data/export-demo-PCQS_Service_Item__c-plan.json -d data/
+
+#echo "*** Creating User"
+#sfdx force:user:create --setalias outlook-user --definitionfile data/user-def.json
+#sfdx force:user:password:generate --targetusername outlook-user
+
+echo "*** Setting up debug mode..."
+sfdx force:apex:execute -f scripts/apex/setDebugMode.apex
+
+echo "*** Setting up Remote Site Settings..."
+#sfdx force:apex:execute -f scripts/apex/createRemoteSiteSettings.apex
+
+echo "*** Opening Org"
+sfdx force:org:open
+#sfdx force:org:open -p /lightning/n/LFO_Demo
