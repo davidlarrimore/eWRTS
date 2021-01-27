ba460561404ca115b7a8444cce7d7f0b0cea6e2d
Dave Larrimore <dlarrimore@salesforce.com>
2021-01-27 12:01:47 -0500

    Stable push to target Org

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,14 +2,13 @@ echo "*** Pushing metadata to scratch org ..."
 sfdx force:source:push
 
 echo "*** Deploying metadata to target org ..."
-sfdx force:source:deploy --targetusername uscis-outlook-lwc --sourcepath force-app
+sfdx force:source:deploy --targetusername eWRTS --sourcepath force-app
 
 echo "*** Deleting Data ..."
-sfdx force:apex:execute -f scripts/apex/deleteData.apex -u uscis-outlook-lwc
+sfdx force:apex:execute -f scripts/apex/deleteData.apex -u eWRTS
 
 echo "*** Re-Importing Data ..."
-sfdx shane:data:tree:import -p data/export-demo-service_item__c-plan.json -d data/ -u uscis-outlook-lwc
-sfdx shane:data:tree:import -p data/export-demo-PCQS_Service_Item__c-plan.json -d data/ -u uscis-outlook-lwc
+sfdx sfdmu:run --sourceusername csvfile --targetusername eWRTS -p data
 
 echo "*** Setting up Remote Site Settings ..."
-sfdx force:apex:execute -f scripts/apex/createRemoteSiteSettings.apex -u uscis-outlook-lwc
+sfdx force:apex:execute -f scripts/apex/createRemoteSiteSettings.apex -u eWRTS
