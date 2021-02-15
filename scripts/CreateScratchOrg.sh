e4a91507be40e4c7fbdfcf348efb782fb00d597c
Dave Larrimore <dlarrimore@salesforce.com>
2021-02-15 14:18:18 -0500

    Date Updates

--- scripts/CreateScratchOrg.sh
+++ scripts/CreateScratchOrg.sh
@@ -13,21 +13,25 @@ echo "*** Pushing metadata to scratch org ..."
 sfdx force:source:push
 
 echo "*** Create Contactor User ..."
-sfdx force:user:create --setalias eWRTSScratch_Contractor email=davidlarrimoresalesforce@gmail.com firstName=Chris lastName=Contractor
+sfdx force:user:create -a eWRTSScratch_Contractor email=davidlarrimoresalesforce@gmail.com firstName=Chris lastName=Contractor -f config/contractor-user-def.json
+sfdx force:user:create -a eWRTSScratch_Analyst email=davidlarrimoresalesforce@gmail.com firstName=Alice lastName=Analyst -f config/analyst-user-def.json
+#sfdx force:user:create -a eWRTSScratch_Analyst2 email=davidlarrimoresalesforce@gmail.com firstName=Steve lastName=Service -f config/analyst-user-def.json
+#sfdx force:user:create -a eWRTSScratch_Analyst3 email=davidlarrimoresalesforce@gmail.com firstName=Bob lastName=Hotchkins -f config/analyst-user-def.json
 
 echo "*** Assigning permission set to your users ..."
 sfdx force:user:permset:assign -n eWRTS_Demo_Admin -u eWRTSScratch
-sfdx force:user:permset:assign -n eWRTS_Contractor -u eWRTSScratch -o eWRTSScratch_Contractor
+
 
 echo "*** Generating password for your users ..."
 sfdx force:user:password:generate --targetusername eWRTSScratch
 sfdx force:user:password:generate --targetusername eWRTSScratch_Contractor
+sfdx force:user:password:generate --targetusername eWRTSScratch_Analyst
 
 echo "*** Creating data"
 sfdx sfdmu:run --sourceusername csvfile --targetusername eWRTSScratch -p data
-sfdx force:apex:execute -f scripts/apex/generateCases.apex
-sfdx force:apex:execute -f scripts/apex/generateCases.apex
-sfdx force:apex:execute -f scripts/apex/generateCases.apex
+#sfdx force:apex:execute -f scripts/apex/randomlyGenerateCases.apex
+#sfdx force:apex:execute -f scripts/apex/randomlyGenerateCases.apex
+#sfdx force:apex:execute -f scripts/apex/randomlyGenerateCases.apex
 
 #echo "*** Creating User"
 #sfdx force:user:create --setalias outlook-user --definitionfile data/user-def.json
--- scripts/exportData.sh
+++ scripts/exportData.sh
@@ -7,16 +7,6 @@ echo "*** Pull Data from Dev..."
 echo "*** Push Data from Scratch CSV to Prod..."
 #sfdx sfdmu:run --sourceusername csvfile --targetusername eWRTS -p data
 
-echo "*** Pull Data from Prod..."
-#sfdx sfdmu:run --sourceusername eWRTS --targetusername csvfile -p data
-
-echo "*** Push Data from Prod CSV to Scratch..."
-#sfdx sfdmu:run --sourceusername csvfile --targetusername eWRTSScratch -p data
-
 echo "*** Push Data from Scratch to Prod..."
 #sfdx sfdmu:run --sourceusername eWRTSScratch --targetusername eWRTS -p data
-
-echo "*** Push Data from Prod to Scratch..."
-#sfdx sfdmu:run --sourceusername eWRTS --targetusername eWRTSScratch -p data
-
 
